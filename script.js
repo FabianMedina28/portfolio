78a79deac417876d1fcde9399bd7c888e795bbb5
FabianMedina28 <medinafabian.2004@gmail.com>
2024-10-18 16:18:18 -0300

    subiendo el responsive tablet y mobile

--- script.js
+++ script.js
@@ -8,32 +8,3 @@ document.getElementById('btnGirar').addEventListener('click', function() {
 document.getElementById('btnGirar2').addEventListener('click', function() {
     document.getElementById('carta').classList.toggle('rotar');
 });
-
-// Inicialización de EmailJS
-(function() {
-    emailjs.init("TU_USER_ID"); // Reemplaza "TU_USER_ID" por tu ID de usuario en EmailJS
-})();
-
-// Función para manejar el envío del formulario
-document.getElementById('contact-form').addEventListener('submit', function(event) {
-    event.preventDefault(); // Prevenir envío predeterminado del formulario
-
-    // Obtener los valores de los campos
-    const name = document.getElementById('name').value;
-    const email = document.getElementById('email').value;
-    const message = document.getElementById('message').value;
-
-    // Enviar el correo utilizando EmailJS
-    emailjs.send("TU_SERVICE_ID", "TU_TEMPLATE_ID", {
-        name: name,
-        email: email,
-        message: message
-    })
-    .then(function(response) {
-        // Muestra un mensaje de éxito
-        alert('Mensaje enviado con éxito!');
-    }, function(error) {
-        // Muestra un mensaje de error
-        alert('Error al enviar el mensaje, inténtalo de nuevo.');
-    });
-});
